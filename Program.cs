9724a061b880f1b2d0073efcd38e3becfe5d60c7
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-05 05:15:59 -0800

    Employee Architectura

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
-﻿using SOLID.ModelSrpSolution;
+﻿using SOLID.EmplyeeArchitecure;
+using SOLID.ModelSrpSolution;
 using SOLID.ModelSRPViolation;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Didn't save SRP
+        Console.WriteLine("Dont Saved SRP");
+        IPhone phone = new Phone();
+        phone.Dial("01234556789");
+        phone.Disconnect();
+        phone.Send("Messege");
+        phone.Recieve();
 
-Console.WriteLine("Dont Saved SRP");
-IPhone phone = new Phone();
-phone.Dial("01234556789");
-phone.Disconnect();
-phone.Send("Messege");
-phone.Recieve();
+        //Saveed SRP
+        Console.WriteLine(new string('_', 100));
+        Console.WriteLine("Saved SRP");
+        PhoneSRP phoneSRP = new(new ConnectionManagerImplementation(), new DataManagerImentation());
+        phone.Send("Message text");
+        phone.Recieve();
+        phone.Dial("0123456789");
+        phoneSRP.Disconnect();
 
-//Save SRP
-Console.WriteLine(new string('_',100));
-Console.WriteLine("Saved SRP");
-PhoneSRP phoneSRP = new(new ConnectionManagerImplementation(), new DataManagerImentation());
-phone.Send("Message text");
-phone.Recieve();
-phone.Dial("0123456789");
-phoneSRP.Disconnect();
+        Console.WriteLine(new string('_', 100));
+        ClientModule.ClientModuleMain();
+    }
+}
